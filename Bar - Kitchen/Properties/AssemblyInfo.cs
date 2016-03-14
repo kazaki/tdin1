859e35832399a0a0bafe5d1906a1ecfb33b71f39
Pedro Fernandes <pedro_fernandes@live.com>
2016-03-14 21:59:06 +0000

    initial commit

---
+++ Bar - Kitchen/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bar - Kitchen")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bar - Kitchen")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("eb0cb263-73cc-4750-93d7-6094a6978a97")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Server/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RestaurantHandler restaurant = new RestaurantHandler();
+
+            restaurant.printTables();
+
+            Console.ReadLine();
+
+            restaurant.addOrder("Pedido exemplo", 10, 0, Order.BAR, 20);
+
+            restaurant.printOrders();
+
+            Console.ReadLine();
+
+            restaurant.printTables();
+
+            Console.ReadLine();
+        }
+    }
+
+    class RestaurantHandler
+    {
+        private IList<Table> Tables;
+        private IList<Order> Orders;
+
+        public RestaurantHandler()
+        {
+            Tables = new List<Table> { new Table(), new Table(), new Table(), new Table(), new Table(), new Table() };
+            Orders = new List<Order> {};
+        }
+
+        public bool addOrder(string description, int quantity, int tableID, int type, int price)
+        {
+
+            Console.WriteLine("[NEW ORDER]");
+            Console.WriteLine("\tDescription: " + description);
+            Console.WriteLine("\tQuantity: " + quantity);
+            Console.WriteLine("\tTable ID: " + tableID);
+            Console.WriteLine("\tType: " + type);
+            Console.WriteLine("\tPrice: " + price);
+
+            Table tbl = Tables.ElementAt(tableID);
+            if (tbl.Occupied == true)
+            {
+                Console.WriteLine("<NEW ORDER DENIED> Table occupied. ");
+                return true;
+            }
+            
+            Orders.Add(new Order(description, quantity, tableID, type, price));
+            tbl.Occupied = true;
+            Console.WriteLine("<NEW ORDER ACCEPTED>");
+            return true;
+        }
+
+        public void printTables()
+        {
+            foreach (var t in Tables)
+            {
+                string status = "FREE";
+                if (t.Occupied) status = "OCCUPIED";
+                Console.WriteLine("[" + t.Id + "] <" + status + ">");
+            }
+        }
+
+        public void printOrders()
+        {
+            Console.WriteLine("Orders:");
+            foreach (var o in Orders)
+            {
+                Console.WriteLine("[" + o.Id + "]");
+                Console.WriteLine("\tDescription: " + o.Description);
+                Console.WriteLine("\tQuantity: " + o.Quantity);
+                Console.WriteLine("\tTableId: " + o.TableId);
+                Console.WriteLine("\tType: " + o.Type);
+                Console.WriteLine("\tPrice: " + o.Price);
+                Console.WriteLine("\tStatus: " + o.Status);
+            }
+        }
+    }
+
+    class Table
+    {
+        private static int LastId = 0;
+
+        public int Id { get; }
+        public bool Occupied { get; set; }
+
+        public Table()
+        {
+            Id = LastId++;
+            Occupied = false;
+        }
+    }
+
+    class Order
+    {
+        public const int BAR = 0, KITCHEN = 1, PENDING = 0, PREPERATION = 1, READY = 2;
+
+        private static int LastId = 0;
+
+        public int Id { get; }
+
+        public string Description { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int TableId { get; }
+
+        public int Type { get; set; } // 0 - Bar, 1 - Kitchen
+
+        public int Price { get; set; }
+
+        public int Status { get; set; } // 0 - Pending, 1 - Preparation, 2 - Ready
+
+        public Order(string description, int quantity, int tableID, int type, int price)
+        {
+            Id = LastId++;
+            Description = description;
+            Quantity = quantity;
+            TableId = tableID;
+            Type = type;
+            Price = price;
+            Status = 0;
+        }
+
+    }
+}
